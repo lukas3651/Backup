4dd9d6db1d62db3dcc4609e907e870d5083a34d7
lukas3651 <126104602+lukas3651@users.noreply.github.com>
2024-12-18 12:07:55 +0000

    Update MovieSeriesBackup.sh

--- Scripts/MovieSeriesBackup.sh
+++ Scripts/MovieSeriesBackup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+git config --global user.email "lukad99.lgh@gmail.com"
+git config --global user.name "lukas3651"
+git config --global --add safe.directory '/mnt/user/Whole/Script Backup'
 ls /mnt/user/Whole/Movies > "/mnt/user/Whole/Script Backup/Filme.txt"
 ls /mnt/user/Whole/Serien > "/mnt/user/Whole/Script Backup/Serien.txt"
 ls /mnt/user/Whole/music > "/mnt/user/Whole/Script Backup/Music.txt"
+tree /mnt/user/Whole/Audiobookshelf/Audiobooks/ -I '*.m4b|*.mp3|*.json|*.jpeg|*.jpg|*.ebup' > "/mnt/user/Whole/Script Backup/Audiobooks.txt"
 sed -i 's/[\/\\]//g' "/mnt/user/Whole/Script Backup/Filme.txt"
 sed -i 's/[\/\\]//g' "/mnt/user/Whole/Script Backup/Serien.txt"
 sed -i 's/[\/\\"]//g; s/(\(.*\)//' "/mnt/user/Whole/Script Backup/Music.txt"
 cd "/mnt/user/Whole/Script Backup"
+git remote set-url origin https://github_token@github.com/lukas3651/Backup.git
 git add "/mnt/user/Whole/Script Backup/Filme.txt"
 git commit -m "Updated Movies Folder"
 git add "/mnt/user/Whole/Script Backup/Serien.txt"
@@ -14,4 +19,6 @@ git add "/mnt/user/Whole/Script Backup/Music.txt"
 git commit -m "Updated Music Folder"
 git add "/mnt/user/Whole/Script Backup/Scripts"
 git commit -m "Updated Scripts Folder"
+git add "/mnt/user/Whole/Script Backup/Audiobooks.txt"
+git commit -m "Updated Audiobooks Folder"
 git push origin master
